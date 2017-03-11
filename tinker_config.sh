a02023bfb4b58212759aeb5070767e9aa1cf3c0c
Jeasine Ma <jeasinema@gmail.com>
2017-03-11 22:41:35 +0800

    [Fix] fix source to ubuntu-ports

--- tinker_config.sh
+++ tinker_config.sh
@@ -57,7 +57,7 @@ filename=$TARGET_ROOTFS_DIR/etc/fstab
 #Add the standard Debian non-free repositories useful to load
 #closed source firmware (i.e. WiFi dongle firmware)
 filename=$TARGET_ROOTFS_DIR/etc/apt/sources.list
-echo deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial main restricted universe multiverse > $filename
+echo deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/ xenial main restricted universe multiverse > $filename
 
 #for enable root login via ssh
 filename=$TARGET_ROOTFS_DIR/etc/ssh/sshd_config
